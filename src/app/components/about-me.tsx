8bb81cc0a93dcffdf7039dd0cf2daae8778064f2
faberjr <hildorjunior@gmail.com>
2025-05-27 11:54:55 -0300

    feat: auto years

--- src/app/components/about-me.tsx
+++ src/app/components/about-me.tsx
@@ -1,4 +1,8 @@
 export default function AboutMe() {
+  const currentYear = new Date().getFullYear();
+  const startingYear = 2016;
+  const yearsOfExperience = currentYear - startingYear;
+
   return (
     <div className='columns is-centered' id='about'>
       <div className='column is-10'>
@@ -9,7 +13,7 @@ export default function AboutMe() {
             (PUCRS) in 2020 and postgraduate in Leadership, Innovation and Management also from PUCRS in 2022.
             <br />
             <br />
-            With more than 6 years of experience in the IT market, I had the opportunity to work in the most diverse
+            With more than {yearsOfExperience} years of experience in the IT market, I had the opportunity to work in the most diverse
             types of projects and companies from small to large. I was able to work on projects in the financial,
             banking and logistics sectors, as well as in the field of health and social events.
             <br />
